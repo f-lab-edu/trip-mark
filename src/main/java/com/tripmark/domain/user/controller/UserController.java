6c7d52466b56024d556adf79d97d487088b6571d
marista-dev <sa21788@naver.com>
2024-11-27 20:33:17 +0900

    #14 feat: 사용자 등록 API 개선
    사용자 등록 API 개선
    - @Controller에서 @RestController로 변경하여 RESTful 지원 강화
    - RestResponse를 사용하여 응답 형식 통일
    - 요청 DTO에 유효성 검증 추가

--- src/main/java/com/tripmark/domain/user/controller/UserController.java
+++ src/main/java/com/tripmark/domain/user/controller/UserController.java
@@ -3,14 +3,16 @@ package com.tripmark.domain.user.controller;
 import com.tripmark.domain.user.dto.UserRegistrationRequestDto;
 import com.tripmark.domain.user.dto.UserRegistrationResponseDto;
 import com.tripmark.domain.user.service.UserService;
+import com.tripmark.global.common.RestResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class UserController {
@@ -18,8 +20,8 @@ public class UserController {
   private final UserService userService;
 
   @PostMapping("/register")
-  public ResponseEntity<UserRegistrationResponseDto> registerUser(@RequestBody UserRegistrationRequestDto requestDto) {
+  public ResponseEntity<RestResponse<UserRegistrationResponseDto>> registerUser(@Valid @RequestBody UserRegistrationRequestDto requestDto) {
     UserRegistrationResponseDto responseDto = userService.registerUser(requestDto);
-    return ResponseEntity.ok(responseDto);
+    return RestResponse.success(responseDto);
   }
 }
